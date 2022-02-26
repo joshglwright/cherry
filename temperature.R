afb7a2bd49edbeacb08fe1f204165a42e843cf7f
Taehoon Song <email@taehoonsong.com>
2022-02-26 16:34:02 -0500

    minor edit

--- temperature.R
+++ temperature.R
@@ -7,22 +7,31 @@ library(here)
 
 i_am('temperature.R')
 
+tmin_dc <- read_csv(here('peak-bloom-prediction','data','statistics_tmin.csv'))
+tmax_dc <- read_csv(here('peak-bloom-prediction','data','statistics_tmax.csv'))
+
+temp_dc <- inner_join(tmin_dc,tmax_dc, by = 'dt',suffix=c('min','max')) %>%
+  mutate(tavg=(value_meanmin+value_meanmax)/2) %>%
+  select(dt,tavg)
 
 # Time series analysis
-avgts<-ts(dc_temp$Temp_Avg,frequency = 365,start=c(1980,1),end=c(2020,365))
+avgts_dc<-ts(temp_dc$tavg,frequency = 365,start=c(1980,1),end=c(2020,365))
 
-ts.plot(avgts)
-plot(decompose(avgts))
+ts.plot(avgts_dc)
 
-par(mfrow=c(2,1))
-acf(avgts, lag = 365)
-pacf(avgts, lag = 365)
+plot(decompose(avgts_dc))
 
 par(mfrow=c(2,1))
-acf(avgts)
-pacf(avgts)
+acf(avgts_dc, lag = 365)
+pacf(avgts_dc, lag = 365)
 
-auto.arima(avgts, ic = 'aic', max.P = 3, max.Q = 3)
+acf(avgts_dc)
+pacf(avgts_dc)
+
+par(mfrow=c(1,1))
+
+auto.arima(avgts_dc, ic = 'aicc', max.P = 3, max.Q = 3, approximation = FALSE, trace = TRUE)
+
 # Result of 
 # Series: avgts 
 # ARIMA(3,0,2)(0,1,0)[365] 
@@ -34,6 +43,8 @@ auto.arima(avgts, ic = 'aic', max.P = 3, max.Q = 3)
 # 
 # sigma^2 = 12.79:  log likelihood = -39321.31
 # AIC=78654.62   AICc=78654.62   BIC=78700.15
-ts_fit<_sarima(avgts, 3,0,2,0,1,0,365)
 
-Temp_Pred<-sarima.for(avgts,4015, 3,0,2,0,1,0,365)
+
+ts_fit <- sarima(avgts_dc, 3,0,2,0,1,0,365)
+
+Temp_Pred <- sarima.for(avgts_dc,4015, 3,0,2,0,1,0,365)
